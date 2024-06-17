b349426d346b583ead683a065fa1c27f40e05ad6
viliger <wowlocale@gmail.com>
2024-06-17 19:06:40 +0300

    Hopefully made Unity files workabale for people who try to import them.

---
+++ RoR2_CatacombsUnity/Assets/Catacombs/Scripts/ContentProvider.cs
@@ -0,0 +1,76 @@
+﻿using RoR2.ContentManagement;
+using System;
+using System.Collections;
+using UnityEngine;
+using Path = System.IO.Path;
+
+namespace DS1Catacombs.Content
+{
+    public class ContentProvider : IContentPackProvider
+    {
+        public string identifier => DS1CatacombsPlugin.GUID + "." + nameof(ContentProvider);
+
+        private readonly ContentPack _contentPack = new ContentPack();
+
+        public static String assetDirectory;
+
+        public IEnumerator LoadStaticContentAsync(LoadStaticContentAsyncArgs args)
+        {
+            _contentPack.identifier = identifier;
+
+            var assetsFolderFullPath = Path.Combine(Path.GetDirectoryName(typeof(ContentProvider).Assembly.Location), "assetbundles");
+            assetDirectory = assetsFolderFullPath;
+
+            var musicFolderFullPath = Path.Combine(Path.GetDirectoryName(typeof(ContentProvider).Assembly.Location), "Soundbanks");
+
+            DS1CatacombsContent.LoadSoundBank(musicFolderFullPath);
+
+            AssetBundle scenesAssetBundle = null;
+            yield return LoadAssetBundle(
+                Path.Combine(assetsFolderFullPath, DS1CatacombsContent.ScenesAssetBundleFileName),
+                args.progressReceiver,
+                (assetBundle) => scenesAssetBundle = assetBundle);
+
+            AssetBundle assetsAssetBundle = null;
+            yield return LoadAssetBundle(
+                Path.Combine(assetsFolderFullPath, DS1CatacombsContent.AssetsAssetBundleFileName),
+                args.progressReceiver,
+                (assetBundle) => assetsAssetBundle = assetBundle);
+
+            yield return DS1CatacombsContent.LoadAssetBundlesAsync(
+                scenesAssetBundle, assetsAssetBundle,
+                args.progressReceiver,
+                _contentPack, musicFolderFullPath);
+
+            yield break;
+        }
+
+        private IEnumerator LoadAssetBundle(string assetBundleFullPath, IProgress<float> progress, Action<AssetBundle> onAssetBundleLoaded)
+        {
+            var assetBundleCreateRequest = AssetBundle.LoadFromFileAsync(assetBundleFullPath);
+            while (!assetBundleCreateRequest.isDone)
+            {
+                progress.Report(assetBundleCreateRequest.progress);
+                yield return null;
+            }
+
+            onAssetBundleLoaded(assetBundleCreateRequest.assetBundle);
+
+            yield break;
+        }
+
+        public IEnumerator GenerateContentPackAsync(GetContentPackAsyncArgs args)
+        {
+            ContentPack.Copy(_contentPack, args.output);
+
+            args.ReportProgress(1f);
+            yield break;
+        }
+
+        public IEnumerator FinalizeAsync(FinalizeAsyncArgs args)
+        {
+            args.ReportProgress(1f);
+            yield break;
+        }
+    }
+}
---
+++ RoR2_CatacombsUnity/Assets/Catacombs/Scripts/DS1CatacombsPlugin.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using RoR2.ContentManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+
+namespace DS1Catacombs
+{
+    [BepInPlugin("com.Viliger.DS1Catacombs", "DS1Catacombs", Version)]
+    [BepInDependency(R2API.DirectorAPI.PluginGUID)]
+    [BepInDependency(R2API.StageRegistration.PluginGUID)]
+    [BepInDependency("com.Viliger.SM64BBF")]
+    //[BepInDependency(R2API.SoundAPI.PluginGUID)]
+    public class DS1CatacombsPlugin : BaseUnityPlugin
+    {
+        public const string Author = "Viliger";
+        public const string Name = nameof(DS1CatacombsPlugin);
+        public const string Version = "0.9.3";
+        public const string GUID = Author + "." + Name;
+
+        public static ConfigEntry<bool> EnableShitpostMusic;
+
+        public static DS1CatacombsPlugin instance;
+        public static PluginInfo PluginInfo;
+
+        private void Awake()
+        {
+            EnableShitpostMusic = Config.Bind("Catacombs", "Enable shitpost music", false, "Enables shitpost music.");
+
+            instance = this;
+            PluginInfo = Info;
+
+            Log.Init(Logger);
+
+            On.RoR2.MusicController.Start += MusicController_Start;
+            On.RoR2.MusicController.LateUpdate += MusicController_LateUpdate;
+            ContentManager.collectContentPackProviders += GiveToRoR2OurContentPackProviders;
+            Language.collectLanguageRootFolders += CollectLanguageRootFolders;
+        }
+
+        private void MusicController_LateUpdate(On.RoR2.MusicController.orig_LateUpdate orig, MusicController self)
+        {
+            bool isPaused = Time.timeScale == 0f;
+            if (isPaused != self.wasPaused)
+            {
+                AkSoundEngine.PostEvent(isPaused ? "DS1_Pause_Music" : "DS1_Unpause_Music", base.gameObject);
+            }
+        }
+
+        private void MusicController_Start(On.RoR2.MusicController.orig_Start orig, MusicController self)
+        {
+            orig(self);
+            AkSoundEngine.PostEvent("DS1_Play_Music_System", self.gameObject);
+        }
+
+        private void Destroy()
+        {
+            Language.collectLanguageRootFolders -= CollectLanguageRootFolders;
+        }
+        private void GiveToRoR2OurContentPackProviders(ContentManager.AddContentPackProviderDelegate addContentPackProvider)
+        {
+            addContentPackProvider(new Content.ContentProvider());
+        }
+        public void CollectLanguageRootFolders(List<string> folders)
+        {
+            folders.Add(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(base.Info.Location), "Language"));
+        }
+    }
+}
